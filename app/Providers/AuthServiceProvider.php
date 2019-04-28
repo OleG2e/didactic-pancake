02c237ea448ec79afa5a36bfce9ff08e92a6e245
OleG2e <oleg2e2@gmail.com>
2019-04-28 22:55:02 +0200

    Bugfix isAdmin()

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -45,8 +45,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::before(function (User $user) {
-            return isAdmin();
+        Gate::before(function () {
+            if (isAdmin()) {
+                return true;
+            }
         });
     }
 }
