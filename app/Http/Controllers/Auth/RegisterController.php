92c1ad99a783e5d80baf6199c72d1dc282a30705
OleG2e <oleg2e2@gmail.com>
2019-05-07 13:49:39 +0200

    Login to the site with email OR name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'without_spaces', 'max:255', 'regex:/^[a-z]+$/i', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'link' => ['required', 'string', 'max:255'],
             'law' => ['required', 'bool'],
